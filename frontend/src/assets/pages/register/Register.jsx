bb9279879cced42e5f48fd8e151295b51a8d07ad
Vishal Patil <vishalpatil6924@gmail.com>
2024-06-18 22:16:06 +0530

    start

--- frontend/src/assets/pages/register/Register.jsx
+++ frontend/src/assets/pages/register/Register.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  
+  return (
+      <div>
+          Register
+      </div>
+  );
 };
 
 export default Register;
