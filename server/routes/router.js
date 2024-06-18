66fd692cd598c868bfc8f571d069417bfebad818
Pranav Patil <pranav07@somaiya.edu>
2024-06-18 23:02:53 +0530

    User schema changed

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv");
 const controllerLogin = require("../controllers/login");
-const multer = require('multer');
+const controllerSignUp = require("../controllers/signup");
+const upload = require("../utils/upload");
+
 
 // Example route
 // router.get("/callback", controllerLogin.callbackCheck);
+
+router.post("/signup", upload.single('file'), controllerSignUp.createAccount);
 
 
 module.exports = router;
